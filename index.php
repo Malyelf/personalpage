7feae053cd4be12e7c693ff5f5b6bd12162586b4
unknown <IT-User@OB01-ITUM-08.CL1C.daa>
2024-11-14 22:30:39 +0100

    funktionale Webseite überarbeitet

--- index.php
+++ index.php
@@ -11,35 +11,10 @@
           <div class="carousel-container">
             <h2 class="animate__animated animate__fadeInDown">Herzlich wilkommen <span>auf meine VPN Seite</span></h2>
             <p class="animate__animated animate__fadeInUp">Entdecken Sie die Vorteile von WireGuard und lernen Sie, wie Sie ganz einfach ein sicheres virtuelles privates Netzwerk (VPN) einrichten.Möchten Sie sicher und anonym im Internet surfen? Mit WireGuard ist das kein Problem. Wir erklären Ihnen, wie's geht</p>
-            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+            <a href="./about.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Mehr Infos</a>
           </div>
-        </div>
-
-       
-        <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-        </a>
-
-        <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-        </a>
-
-      </div>
-
-      <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
-        <defs>
-          <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
-        </defs>
-        <g class="wave1">
-          <use xlink:href="#wave-path" x="50" y="3"></use>
-        </g>
-        <g class="wave2">
-          <use xlink:href="#wave-path" x="50" y="0"></use>
-        </g>
-        <g class="wave3">
-          <use xlink:href="#wave-path" x="50" y="9"></use>
-        </g>
-      </svg>
+        
+     
 
     </section><!-- /Hero Section -->
   </main>
