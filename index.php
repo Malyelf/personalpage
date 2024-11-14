4ac4a6090946dbd2f4e06c5496b61f7183e4a431
unknown <IT-User@OB01-ITUM-08.CL1C.daa>
2024-11-14 19:38:19 +0100

    funktionale Webseite abgeschlossen

--- index.php
+++ index.php
@@ -43,40 +43,7 @@
 
     </section><!-- /Hero Section -->
 
-   <!-- About Section -->
-   <section id="about" class="about section">
-
-<div class="container">
-
-  <div class="row position-relative">
-
-    <div class="col-lg-7 about-img" data-aos="zoom-out" data-aos-delay="200"><img src="assets/img/about.jpg"></div>
-
-    <div class="col-lg-7" data-aos="fade-up" data-aos-delay="100">
-      <h2 class="inner-title">Was ist WireGuard</h2>
-      <div class="our-story">
-        <h4>WireGuard</h4>
-        <h3>Die Geschichte</h3>
-        <p>WireGuard ist ein modernes VPN-Protokoll, das 2016 veröffentlicht wurde und in den letzten Jahren immer beliebter geworden ist. Im Vergleich zu älteren Protokollen wie OpenVPN oder IPSec ist WireGuard deutlich einfacher aufgebaut und bietet eine bessere Performance.</p>
-        <ul>
-          <li><i class="bi bi-check-circle"></i> <span> WireGuard ist bekannt für seine hohe Geschwindigkeit.</span></li>
-          <li><i class="bi bi-check-circle"></i> <span>Es nutzt starke Verschlüsselungsalgorithmen</span></li>
-          <li><i class="bi bi-check-circle"></i> <span>WireGuard ist im Vergleich zu anderen VPN-Protokollen deutlich einfacher aufgebaut.</span></li>
-        </ul>
-        Wenn du Wert auf eine schnelle, sichere und einfach zu bedienende VPN-Verbindung legst, ist WireGuard eine ausgezeichnete Wahl. Es eignet sich sowohl für private Nutzer als auch für Unternehmen.
-        <div class="watch-video d-flex align-items-center position-relative">
-          <i class="bi bi-play-circle"></i>
-          <a href="https://www.youtube.com/watch?v=BVhJ35KqqlA" class="glightbox stretched-link">Sehe Video</a>
-        </div>
-      </div>
-    </div>
-
-  </div>
-
-</div>
-
-</section><!-- /About Section -->
- 
+   
 
   
   
